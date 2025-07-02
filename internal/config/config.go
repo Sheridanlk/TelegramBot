7400973d1b85c984c85e09f0a833998c72a98821
sasha <sasha_kobzev18@mail.ru>
2025-07-03 02:01:33 +0300

    feature: add config

---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Token string `yaml:"bot_token" env-default:""`
+}
+
+// Must означает что функция не возвращает ошибку, а паникует
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	//check is file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exists: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
